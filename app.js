8ec87ab79f47f65a9a86f4fe424ecd1f22c05039
narsi <nmupphere@gmail.com>
2018-07-04 17:22:48 -0500

    Add webpack dev server
    Web pack dev server

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const webpack = require('webpack');
 const indexRouter = require('./routes/index');
-
-const app = express();
-
-app.set('views', process.cwd() + '/views')
-app.set('view engine','pug')
-app.use('/dist', express.static('./dist'))
-
-app.use('/', indexRouter);
-
+const WebpackDevServer = require('webpack-dev-server')
+const config = require("./webpack.config.js")('',{mode: 'development'});
+const compiler = webpack(config);
+const app = new WebpackDevServer(compiler, {
+        stats: {colors: true},
+        noInfo: false,
+        before: function(app) {
+            app.set('views', process.cwd() + '/views')
+            app.set('view engine','pug')
+            app.use('/dist', express.static('./dist'))
+            app.use('/', indexRouter);
+        }
+    });
 module.exports = app;
--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 const app = require('./app')
-const webpackConfig = require('./webpack.config')
-
-if(process.env.NODE_ENV !== 'production') {
-    console.log('dev mode')
-    const webpackDevMiddleware = require('webpack-dev-middleware')
-    const webpack = require('webpack')
-    const middlewareOptions = {
-        stats: {colors: true},
-        noInfo: false, // Comment this out for more verbose webpack information
-    }
-    app.use(webpackDevMiddleware(webpack(webpackConfig('',{mode: 'development'})), middlewareOptions))
-
-}
 
 const port = process.env.PORT || 3001
 
-app.listen(port)
-
-console.log('Server started at port', port)
+app.listen(port, "localhost", () => {
+    console.log('Server started at port', port)
+})
