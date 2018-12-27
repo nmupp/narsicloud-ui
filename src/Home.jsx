83e0c76bc701e968c5a1ff664f280d474ff6a78d
Narasimhulu Muppalla <nmuppalla@salesforce.com>
2018-12-27 13:23:26 -0500

    add app bar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import { withTheme } from '@material-ui/core/styles';
 
-const Home = () => {
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
+
+const Home = (props) => {
+    const {classes} = props
     return (
-        <div>
-            <strong>Welcome narsicloud excellent123!!!</strong>
-            <Button variant="contained" color="primary">
-                Hello World123!!
-            </Button>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                    narsicloud
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+
+          </AppBar>            
         </div>
     )
 }
 
-export default withTheme()(Home)
+Home.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Home)
